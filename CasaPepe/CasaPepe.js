7cc4c6b4f6bb410be31f7349fb8d87fc79bf7025
Jessie Wong <kimjang.wong1@gmail.com>
2019-11-19 16:54:33 +0000

    added CasaPepe.js

---
+++ CasaPepe/CasaPepe.js
@@ -0,0 +1,103 @@
+{
+	"restaurantmenu": {
+		"section": [
+			{
+				"entree": [
+					{
+						"item": "Steamed Mussels in Garlic and Wine Sauce",
+						"price": "12.00"
+					},
+					{
+						"item": "Buffalo Mozzarella Caprese",
+						"price": "8.00"
+					},
+					{
+						"item": "Salad of The Day",
+						"price": "8.00"
+					},
+					{
+						"item": "Minestrone Soup",
+						"price": "9.00"
+					}
+				],
+				"_name": "Starters"
+			},
+			{
+				"entree": [
+					{
+						"item": "Fresh Fish of The Day",
+						"price": "19.50"
+					},
+					{
+						"item": "Chicken Parmigiano",
+						"price": "16.50"
+					},
+					{
+						"item": "Linguine alle Vangole",
+						"price": "14.55",
+						"_vegetarian": "true"
+					},
+					{
+						"item": "Gnocchi Pomodoro",
+						"price": "10.95",
+						"_vegetarian": "true"
+					},
+					{
+						"item": "Vitello Marsala",
+						"price": "17.95",
+						"_vegetarian": "true"
+					},
+					{
+						"item": "Risotto di Funghi",
+						"price": "18.00",
+						"_vegetarian": "true"
+					},
+					{
+						"item": "Costata di Manzo",
+						"price": "22.00"
+					}
+				],
+				"_name": "Main Courses"
+			},
+			{
+				"entree": [
+					{
+						"item": "Garlic Bread",
+						"price": "4.85"
+					},
+					{
+						"item": "Onion Rings",
+						"price": "3.85"
+					},
+					{
+						"item": "Fries",
+						"price": "2.95"
+					},
+					{
+						"item": "Cheese Fries",
+						"price": "4.95"
+					}
+				],
+				"_name": "Sides"
+			},
+			{
+				"entree": [
+					{
+						"item": "Tiramisu Cheesecake",
+						"price": "4.95"
+					},
+					{
+						"item": "Carrot Cake",
+						"price": "4.75",
+						"_vegetarian": "true"
+					},
+					{
+						"item": "Selection of Ice-Cream",
+						"price": "4.25"
+					}
+				],
+				"_name": "Desserts"
+			}
+		]
+	}
+}
