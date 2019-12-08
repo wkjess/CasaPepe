11ccc1584bca02610e921e287deef0f2d147dbed
Jessie Wong <kimjang.wong1@gmail.com>
2019-12-08 02:18:12 +0000

    modified files

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-// All the codes below from lecturer, Mikhail Timofeev
+var http = require('http'), //This module provides the HTTP server functionalities
+    path = require('path'), //The path module provides utilities for working with file and directory paths
+    express = require('express'), //This module allows this app to respond to HTTP Requests, defines the routing and renders back the required content
+    fs = require('fs'), //This module allows to work witht the file system: read and write files back
+    xmlParse = require('xslt-processor').xmlParse, //This module allows us to work with XML files
+    xsltProcess = require('xslt-processor').xsltProcess, //The same module allows us to utilise XSL Transformations
+    xml2js = require('xml2js'); //This module does XML to JSON conversion and also allows us to get from JSON back to XML
 
-var http = require('http'),     path = require('path'), 
-    express = require('express'),
-    fs = require('fs'), 
-    xmlParse = require('xslt-processor').xmlParse, 
-    xsltProcess = require('xslt-processor').xsltProcess, 
-    xml2js = require('xml2js'); 
+var router = express(); //The set our routing to be handled by Express
+var server = http.createServer(router); //This is where our server gets created
 
-var router = express(); 
-var server = http.createServer(router);
-
-router.use(express.static(path.resolve(__dirname, 'views'))); 
-router.use(express.urlencoded({extended: true})); 
-router.use(express.json()); 
+router.use(express.static(path.resolve(__dirname, 'views'))); //We define the views folder as the one where all static content will be served
+router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be coming in as part of the URL in GET and POST requests
+router.use(express.json()); //We include support for JSON that is coming from the client
 
 // Function to read in XML file and convert it to JSON
 function xmlFileToJs(filename, cb) {
@@ -63,7 +62,7 @@ router.post('/post/json', function(req, res) {
     xmlFileToJs('CasaPepe.xml', function(err, result) {
       if (err) throw (err);
       //This is where you pass on information from the form inside index.html in a form of JSON and navigate through our JSON (XML) file to create a new entree object
-      result.cafemenu.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price}); //If your XML elements are differet, this is where you have to change to your own element names
+      result.restaurantmenu.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price}); //If your XML elements are differet, this is where you have to change to your own element names
       //Converting back to our original XML file from JSON
       jsToXmlFile('CasaPepe.xml', result, function(err) {
         if (err) console.log(err);
@@ -88,7 +87,7 @@ router.post('/post/delete', function(req, res) {
     xmlFileToJs('CasaPepe.xml', function(err, result) {
       if (err) throw (err);
       //This is where we delete the object based on the position of the section and position of the entree, as being passed on from index.html
-      delete result.cafemenu.section[obj.section].entree[obj.entree];
+      delete result.restaurantmenu.section[obj.section].entree[obj.entree];
       //This is where we convert from JSON and write back our XML file
       jsToXmlFile('CasaPepe.xml', result, function(err) {
         if (err) console.log(err);
